ebf18f8210033b59757fccd60b6dd04b3bfce4c0
András Veres-Szentkirályi <vsza@vsza.hu>
2012-07-20 11:36:23 +0200

    android create project

---
+++ src/hu/silentsignal/android/nfcat/Main.java
@@ -0,0 +1,15 @@
+package hu.silentsignal.android.nfcat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
