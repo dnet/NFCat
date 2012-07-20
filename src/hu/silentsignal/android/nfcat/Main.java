c26b721205115f57bb5fe666e16a83ca8233fcf5
András Veres-Szentkirályi <vsza@vsza.hu>
2012-07-20 11:52:49 +0200

    added NFC intent handler

--- src/hu/silentsignal/android/nfcat/Main.java
+++ src/hu/silentsignal/android/nfcat/Main.java
@@ -2,14 +2,68 @@ package hu.silentsignal.android.nfcat;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.*;
+import android.app.*;
+import android.nfc.*;
+import android.nfc.tech.MifareClassic;
+import java.io.IOException;
 
 public class Main extends Activity
 {
+    protected NfcAdapter mAdapter;
+    protected IntentFilter[] mFilters;
+    protected final static String[][] mTechLists = new String[][] { new String[] { MifareClassic.class.getName() } };
+    protected PendingIntent mPendingIntent;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        mAdapter = NfcAdapter.getDefaultAdapter(this);
+        mPendingIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        final IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
+
+        try {
+            ndef.addDataType("*/*");
+        } catch (IntentFilter.MalformedMimeTypeException e) {
+            throw new RuntimeException("fail", e);
+        }
+        mFilters = new IntentFilter[] {
+            ndef,
+        };
+
+        final Intent intent = getIntent();
+        resolveIntent(intent);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mAdapter.enableForegroundDispatch(this, mPendingIntent, mFilters, mTechLists);
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        resolveIntent(intent);
+    }
+
+    void resolveIntent(Intent intent) {
+        final String action = intent.getAction();
+
+        if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
+            Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            MifareClassic mfc = MifareClassic.get(tagFromIntent);
+            // TODO
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mAdapter.disableForegroundDispatch(this);
     }
 }
